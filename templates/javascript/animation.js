098020b4ff829adf5c7139460a1c982076e874b3
Dennis Haulund Nielsen <dhni@Denniss-MacBook-Pro.local>
2016-03-07 11:22:03 +0100

    feat(generators): Added a new animation generator, for creating javascript based animations

---
+++ templates/javascript/animation.js
@@ -0,0 +1,48 @@
+(function () {
+	'use strict';
+
+	/**
+	 * @name
+	 * @author
+	 * @description
+	 *
+	 */
+	angular
+		.module('<%= moduleName %>')
+		.animation('.<%= dashedName %>', <%= classedName %>);
+
+	/* @ngInject */
+	function <%= classedName %>() {
+		/*jshint validthis: true */
+		var animationDefinition = {
+			//enter: enterFn,
+			//leave: leaveFn,
+			//move: moveFn,
+			//addClass: addClassFn,
+			//removeClass: removeClassFn,
+			//animate: animateFn
+		};
+
+		return animationDefinition;
+
+		/*
+		function enterFn(element, done) {
+			element.css('opacity',0);
+
+			jQuery(element).animate({
+				opacity: 1
+			}, done);
+
+			// optional onDone or onCancel callback
+			// function to handle any post-animation
+			// cleanup operations
+		 	return function(isCancelled) {
+		 		if(isCancelled) {
+		 			jQuery(element).stop();
+		 		}
+		 	}
+		}
+		 */
+	}
+
+})();
